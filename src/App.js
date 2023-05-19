ba750661006e52d35bbd92e5612baa8d3030ada1
Nishant Jha <nkj.nishant@gmail.com>
2023-05-19 22:40:25 +0530

    changed api calls from older method to new method

--- src/App.js
+++ src/App.js
@@ -38,8 +38,63 @@ class App extends Component {
     };
   }
 
+  onButtonSubmit = () => {
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = '6b8400558e134bffb6eb548e1298ed9d';
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = 'bpw161z89otb';
+    const APP_ID = 'visageer';
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'face-detection';
+    const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+    const IMAGE_URL = this.state.input;
+
+    this.setState({
+      imageUrl: this.state.input
+    })
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+    ///////////////////////////////////////////////////////////////////////////////////
+
+    const raw = JSON.stringify({
+          "user_app_id": {
+              "user_id": USER_ID,
+              "app_id": APP_ID
+          },
+          "inputs": [
+              {
+                  "data": {
+                      "image": {
+                          "url": IMAGE_URL
+                      }
+                  }
+              }
+          ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+    // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+    // this will default to the latest version_id
+
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+        .then(response => response.json())
+        .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+        .catch(error => console.log('error', error));
+  }
+
   calculateFaceLocation = (data) =>{
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    let clarifaiFace = data?.outputs[0]?.data?.regions[0]?.region_info?.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
@@ -62,20 +117,20 @@ class App extends Component {
     this.setState({input: event.target.value})
   };
 
-  onButtonSubmit = () => {
-    this.setState({
-      imageUrl: this.state.input,
-    })
-    // console.log("click");
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL, 
-        this.state.input
-      )
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response))
-      .catch(err => console.log(err))
-      )
-  };
+  // onButtonSubmit = () => {
+  //   this.setState({
+  //     imageUrl: this.state.input,
+  //   })
+  //   // console.log("click");
+  //   app.models
+  //     .predict(
+  //       Clarifai.FACE_DETECT_MODEL, 
+  //       this.state.input
+  //     )
+  //     .then(response => this.displayFaceBox(this.calculateFaceLocation(response))
+  //     .catch(err => console.log(err))
+  //     )
+  // };
 
   onRouteChange = (route) => {
     if(route === 'signout'){

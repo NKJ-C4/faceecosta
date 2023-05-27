22d6576b8a2dd215d18c2412da2b605e9919f1d8
Nishant Jha <nkj.nishant@gmail.com>
2023-05-28 01:44:23 +0530

    shifted clarifai to backend

--- src/App.js
+++ src/App.js
@@ -68,49 +68,18 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    // Your PAT (Personal Access Token) can be found in the portal under Authentification
-    const PAT = '6b8400558e134bffb6eb548e1298ed9d';
-    const USER_ID = 'bpw161z89otb';
-    const APP_ID = 'visageer';
-    const MODEL_ID = 'face-detection';
-    const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
-    const IMAGE_URL = this.state.input;
-
     this.setState({
       imageUrl: this.state.input
     })
 
-    const raw = JSON.stringify({
-          "user_app_id": {
-              "user_id": USER_ID,
-              "app_id": APP_ID
-          },
-          "inputs": [
-              {
-                  "data": {
-                      "image": {
-                          "url": IMAGE_URL
-                      }
-                  }
-              }
-          ]
-    });
-
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Authorization': 'Key ' + PAT
-        },
-        body: raw
-    };
-
-    // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
-    // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
-    // this will default to the latest version_id
-
-    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
-        .then(response => response.json())
+    fetch('http://localhost:3000/imageurl', {
+      method: 'post',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+      .then(response => response.json())
         .then(response => {
           if(response) {
             fetch('http://localhost:3000/image/', {
